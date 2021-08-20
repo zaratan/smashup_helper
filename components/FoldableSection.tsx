e95898e020cf99f2406584ca877a527f0541b4de
Denis Zaratan Pasin <zaratan@hey.com>
2021-08-20 21:11:39 +0900

    Openes of sections is stored in LS

--- components/FoldableSection.tsx
+++ components/FoldableSection.tsx
@@ -3,6 +3,50 @@ import { Disclosure } from '@headlessui/react';
 import SectionSeparator from './SectionSeparator';
 import { classNames } from '../helpers/cssClasses';
 
+const WrappedSection = ({
+  open,
+  children,
+  title,
+  className,
+  close,
+}: {
+  open: boolean;
+  children: ReactNode;
+  title: string;
+  className?: string;
+  close: () => void;
+}) => {
+  useEffect(() => {
+    const lsOpen = JSON.parse(
+      window.localStorage.getItem(`FoldableSection:${title}`)
+    );
+
+    if (lsOpen === false) {
+      close();
+    }
+  }, [close, title]);
+
+  useEffect(() => {
+    console.log('LOL');
+    window.localStorage.setItem(
+      `FoldableSection:${title}`,
+      JSON.stringify(open)
+    );
+  }, [open, title]);
+
+  return (
+    <section className={classNames(className, 'mb-6 max-w-6xl mx-auto')}>
+      <SectionSeparator
+        title={title}
+        hidden={!open}
+        className="mb-10"
+        foldButton
+      />
+      <Disclosure.Panel>{children}</Disclosure.Panel>
+    </section>
+  );
+};
+
 function FoldableSection({
   title,
   children,
@@ -12,30 +56,17 @@ function FoldableSection({
   children: ReactNode;
   className?: string;
 }) {
-  const [defaultOpen, setDefaultOpen] = useState(true);
-
-  useEffect(() => {
-    const lsOpen = JSON.parse(
-      window.localStorage.getItem(`FoldableSection:${title}`)
-    );
-
-    if (lsOpen === false) {
-      setDefaultOpen(false);
-    }
-  }, [title]);
-
   return (
-    <Disclosure defaultOpen={defaultOpen}>
-      {({ open }) => (
-        <section className={classNames(className, 'mb-6 max-w-6xl mx-auto')}>
-          <SectionSeparator
-            title={title}
-            hidden={!open}
-            className="mb-10"
-            foldButton
-          />
-          <Disclosure.Panel>{children}</Disclosure.Panel>
-        </section>
+    <Disclosure defaultOpen>
+      {({ open, close }) => (
+        <WrappedSection
+          open={open}
+          title={title}
+          className={className}
+          close={close}
+        >
+          {children}
+        </WrappedSection>
       )}
     </Disclosure>
   );
